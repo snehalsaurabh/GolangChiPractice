9863cd49814d7d81dd0a5ef6220958f88a4f0248
Snehal Saurabh <snehalsaurabh.code@gmail.com>
2024-06-01 15:56:38 +0530

    Refactored code and routes

---
+++ handler/order.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Order struct{}
+
+func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Order Created")
+}
+
+func (o *Order) List(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("List of Orders Updated")
+}
+
+func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get an order by ID")
+}
+
+func (o *Order) UpdateByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update an order by ID")
+}
+
+func (o *Order) DeleteByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Delete an order by ID")
+}
--- main.go
+++ main.go
@@ -1,28 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/snehalsaurabh/GolangChiPractice/application"
 )
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-
-	router.Get("/hello", basicHandler)
-
-	server := &http.Server{	
-		Addr: ":3000",
-		Handler: router,
-	}
-	err := server.ListenAndServe()
+	app := application.New()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("Failed to start server: ", err)
+		fmt.Println("failed to start application", err)
 	}
-
 }
 
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
-}
+
